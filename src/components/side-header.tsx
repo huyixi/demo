4aa78c7fd68c770f9d7f46888be76805c42bcced
huyixi <hi@huyixi.com>
2024-11-01 13:21:54 +0800

    feat: add icons component

--- src/components/side-header.tsx
+++ src/components/side-header.tsx
@@ -1,7 +1,17 @@
 import Link from "next/link";
 
 import { siteConfig } from "@/app/config/site";
+import { Icons } from "./icons";
 
 export default function SiteHeader() {
-  return <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"></header>;
+  return (
+    <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+        <Icons.gitHub className="h-6 w-6" />
+        <span className="sr-only">GitHub</span>
+        <Icons.logo className="h-6 w-6" />
+        <Icons.shadcn className="h-6 w-6" />
+      </Link>
+    </header>
+  );
 }
